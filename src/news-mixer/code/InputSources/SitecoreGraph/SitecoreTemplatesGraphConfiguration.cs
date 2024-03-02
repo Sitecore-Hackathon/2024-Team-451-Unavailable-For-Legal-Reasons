de3e011ae74d1317df69e6714e0ab388bbbc5cdc
jesper.balle <jesper.balle@valtech.com>
2024-03-02 12:30:00 +0100

    Initial news-mixer implementation

---
+++ src/news-mixer/code/InputSources/SitecoreGraph/SitecoreTemplatesGraphConfiguration.cs
@@ -0,0 +1,45 @@
+﻿namespace NewsMixer.InputSources.SitecoreGraph
+{
+    public class SitecoreTemplatesGraphConfiguration : SitecoreGraphInputConfiguration
+    {
+        public Guid RootItemId {  get; set; }
+
+        public Guid TemplateId { get; set; }
+
+        public string? Language { get; set; }
+        
+        public string? TitleField { get; set; }
+
+        public string? ContentField { get; set; }
+
+        public new string Query
+        {
+            get {
+                return GetQueryTemplate()
+                    .Replace("{rootItemId}", RootItemId.ToString("N"))
+                    .Replace("{templateId}", TemplateId.ToString("N"))
+                    .Replace("{language}", Language)
+                    .Replace("{titleField}", TitleField)
+                    .Replace("{contentField}", ContentField);
+            }
+            set { }
+        }
+
+        private static string? _template = null;
+
+        private static string GetQueryTemplate()
+        {
+            if(_template != null)
+            {
+                return _template;
+            }
+
+            var t = typeof(SitecoreTemplatesGraphConfiguration);
+            var resourceName = t.FullName.Replace(t.Name, "SitecoreTemplatesGraph.graphql");
+            using var stream = typeof(SitecoreTemplatesGraphConfiguration).Assembly.GetManifestResourceStream(resourceName)!;
+            using var reader = new StreamReader(stream);
+            _template = reader.ReadToEnd();
+            return _template;
+        }
+    }
+}
---
+++ src/news-mixer/unittest/InputSources/SitecoreGraph/SitecoreTemplatesGraphConfigurationShould.cs
@@ -0,0 +1,26 @@
+﻿using NewsMixer.InputSources.SitecoreGraph;
+
+namespace NewsMixer.UnitTests.InputSources.SitecoreGraph
+{
+    public class SitecoreTemplatesGraphConfigurationShould
+    {
+        [Fact]
+        public void CreateQueryWithInterpolation()
+        {
+            var config = new SitecoreTemplatesGraphConfiguration
+            {
+                ContentField = "contentfieldname",
+                TitleField = "titlefieldname",
+                Language = "sk",
+                TemplateId = new Guid("A0000000-0000-0000-0000-000000000001"),
+                RootItemId = new Guid("B0000000-0000-0000-0000-000000000002"),
+            };
+
+            var actual = config.Query;
+
+            Assert.Contains("title: titlefieldname { value }", actual);
+            Assert.Contains("content: contentfieldname { value }", actual);
+            Assert.Contains("{ name: \"_language\",  value: \"sk\" },", actual);
+        }
+    }
+}
