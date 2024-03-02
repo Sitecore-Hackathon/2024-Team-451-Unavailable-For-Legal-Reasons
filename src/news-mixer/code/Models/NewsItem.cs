707d61f6e232b8b2d5069aec50bcb45227744e59
jesper.balle <jesper.balle@valtech.com>
2024-03-02 12:52:44 +0100

    Skeleton implementation

---
+++ src/news-mixer/code/Models/NewsItem.cs
@@ -0,0 +1,11 @@
+﻿namespace NewsMixer.Models
+{
+    public class NewsItem
+    {
+        public ICollection<string>? Categories { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Url { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
--- src/news-mixer/code/Program.cs
+++ src/news-mixer/code/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NewsMixer.InputSources.DummySource;
+using NewsMixer.Output.Console;
+using NewsMixer.Transforms.DummySummary;
+
+Console.WriteLine("This is the NewsMixer!");
+Console.WriteLine("Error 451 Unavailable for Legal Resons");
+Console.WriteLine("");
+
+var source = new DummySourceInput();
+var transform = new DummySummaryTransform();
+var output = new ConsoleOutput();
+
+var token = new CancellationToken();
+var enumerable1 = source.Execute(token);
+var enumerable2 = transform.Execute(enumerable1, token);
+var result = output.Execute(enumerable2, token);
+
+await result;
