80ca72bc3b3d09e2dedacfc8ba98296661fd856f
jesper.balle <jesper.balle@valtech.com>
2024-03-02 20:57:52 +0100

    Initial skeleton for Search Source

---
+++ src/news-mixer/code/InputSources/SitecoreSearch/SitecoreSearchSource.cs
@@ -0,0 +1,39 @@
+﻿using NewsMixer.Models;
+using System.Runtime.CompilerServices;
+
+namespace NewsMixer.InputSources.SitecoreSearch
+{
+    public class SitecoreSearchConfiguration
+    {
+        public Uri SearchEndpoint { get; set; } = new Uri("https://discover.sitecorecloud.io/discover/v2/");
+
+        public string? SearchDomainId { get; set; } = "43533744";
+
+        public string? SearchApiKey { get; set; } = "01-5370450c-b5ef9b415170c314ee8bc9c99d4f51410f601cb8";
+
+        public string ContextLocale { get; set; } = "en-us";
+
+        public string ContextPage { get; set; } = "/search-results";
+
+        public string WidgetRfkId { get; set; } = "rfkid_7";
+
+        public string Entity { get; set; } = "content";
+
+        public string? QueryPhrase { get; set; }
+
+        public string Sort { get; set; } = "featured";
+
+        public string[] Sources { get; set; } = [];
+
+        public bool FetchPageContent { get; set; } = true;
+
+    }
+
+    public class SitecoreSearchSource : ISourceInput
+    {
+        public async IAsyncEnumerable<NewsItem> Execute([EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            yield break;
+        }
+    }
+}
